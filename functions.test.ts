977ac2cb24f4ceb09229dc2df68363b8d755fd82
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-25 10:39:36 -0700

    Unit testing

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,23 @@
 const {shuffleArray} = require('./utils')
 
-describe('shuffleArray should', () => {
-    // CODE HERE
+
+
+
+
+
+test("items should be shuffled", ()=>{
+    const shuf= [1,2,3,4,5]
+    expect(shuffleArray([1,2,3,4,5])).not.toEqual((shuf))
+
+})
+
+test("test the length", ()=>{
+    const random = shuffleArray([55,234,4,1345,3,42,54,3]);
+    expect(random).toHaveLength(8);
+})
+
+test("items are the same in the array", ()=>{
+    const items = [1,2,3,4,5,6,7]
+    expect(shuffleArray([7,6,5,4,3,2,1])).toEqual(expect.arrayContaining(items))
+
 })
