5fb96359de8804a7283605dd5b25818279881985
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-25 11:13:15 -0700

    duelDuo test

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -5,12 +5,38 @@ require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
+
+
+
+
 beforeEach(async () => {
     driver.get('http://localhost:3000/')
 })
 
 afterAll(async () => {
     driver.quit()
+})
+
+
+test(' draw button displays the div',async () => {
+    const button = await driver.findElement(By.id('draw'))
+    await button.click()
+    const display = await driver.findElement(By.id('choices'))
+    const choices = await display.isDisplayed()
+    expect(choices).toBe(true)
+    
+})
+
+test("removed from duo returns to choices", async ()=>{
+    const draw = await driver.findElement(By.id('draw'))
+    await draw.click()
+    const button = await driver.findElement(By.className('bot-btn'))
+    await button.click()
+    const display = await  driver.findElement(By.id('player-duo'))
+    const choices = await display.isDisplayed()
+    expect(choices).toBe(true)
+
+    await driver.sleep(1000)
 })
 
 test('Title shows up when page loads', async () => {
