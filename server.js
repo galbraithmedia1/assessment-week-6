3bad0ef89eb5e59db1e583b142e5a7f88fa33ddf
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-25 11:16:24 -0700

    update

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const {shuffleArray} = require('./utils')
 app.use(express.json())
 
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '9e70a7c617974590af369af5fcec910e',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
